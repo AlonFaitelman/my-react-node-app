2a64b0915a19b6258c2333555097e18b7cbbf555
unknown <alonfa2203@gmail.com>
2025-01-13 14:09:38 +0200

    node and react works and communicatint each other

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
